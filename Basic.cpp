e9165b011ea72b6a9db9602294dea231a39ad8a1
ramireddygayathri <gayathri200302@gmail.com>
2024-12-17 13:06:02 +0530

    intial

--- Basic.cpp
+++ Basic.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >> name;
     cout << "Enter Number: ";
     cin >> number;
-    cout << "Hello " << name <<" your favourite number is "<<number;
+    cout << "Hello hey " << name <<" your favourite number is "<<number;
     return 0;
 }
 
