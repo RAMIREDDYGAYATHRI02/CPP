f83fe124ed8d1c42a3ba20da893283d4acd2db73
ramireddygayathri <gayathri200302@gmail.com>
2024-12-18 20:17:35 +0530

    reverse

---
+++ Basic/reverse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int remainder,reverse_number=0,n;
+    cout<<"Enter a number:";
+    cin>>n;
+    while(n!=0)
+    {
+        remainder=n%10;
+        reverse_number=reverse_number*10+remainder;
+        n=n/10;
+    }
+    cout<<"The reverse number is :"<<reverse_number;
+}
