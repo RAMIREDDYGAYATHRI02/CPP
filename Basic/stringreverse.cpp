a0219e9fc342670ff7a4e0d39981bf3a491648b3
ramireddygayathri <gayathri200302@gmail.com>
2024-12-18 20:44:12 +0530

    rev

--- Basic/stringreverse.cpp
+++ Basic/stringreverse.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     string str;
-    cout << "Enter a string: ";
+    cout << "Enter a string:";
     cin >> str;
     reverse(str.begin(), str.end());
     cout << "Reversed string: " << str;
