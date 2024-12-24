b3001343e2248f5d83ee0d23de3b3e1204f15848
ramireddygayathri <gayathri200302@gmail.com>
2024-12-24 19:39:58 +0530

    basic

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main() {
     int number;
+    string str;
     float num;
     string name;
     cout << "Enter Name: ";
     cin >> name;
+    cout<<"Enter u r fav name: ";
+    cin>>str;
     cout << "Enter Number: ";
     cin >> number;
     cout << "Enter Number with floting: ";
     cin >> num;
-    cout << "Hello heyy " << name <<" your favourite number is "<<number<<" and "<<num;
+    cout << "Hello heyy " << name <<" your favourite number is "<<number<<" and "<<num <<"and fav name is "<<str;
+    
     return 0;
 }
 
